018a0f8a8cc125b6efd3999a8b149442bce3ab6e
fah <farhan.abuhazan@gmail.com>
2019-12-12 18:09:28 +0700

    check os before open chrome browser

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@
 
 import childProc from 'child_process'
 import server from './createServer'
+
 const port = 4002
 
 server.listen(port, () => {
+
   console.log(`🚀 Server ready at http://localhost:${port}`)
-  childProc.exec('open -a "Google Chrome" http://localhost:4002/', () => null);
+  if (process.platform === 'darwin' && process.env.NODE_ENV === 'production') {
+    childProc.exec('open -a "Google Chrome" http://localhost:4002/', () => null)
+  }
 })
