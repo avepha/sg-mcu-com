097c52f8a28c7a94d2d612e2ed2cc21f578903b2
fah <farhan.abuhazan@gmail.com>
2019-12-12 11:05:13 +0700

    open chrome after init

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 "use strict";
 
+var _child_process = _interopRequireDefault(require("child_process"));
+
 var _createServer = _interopRequireDefault(require("./createServer"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -9,4 +11,6 @@ const port = 4002;
 
 _createServer.default.listen(port, () => {
   console.log(`🚀 Server ready at http://localhost:${port}`);
+
+  _child_process.default.exec('open -a "Google Chrome" http://localhost:4002/', () => null);
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+import childProc from 'child_process'
 import server from './createServer'
 const port = 4002
 
 server.listen(port, () => {
   console.log(`🚀 Server ready at http://localhost:${port}`)
+  childProc.exec('open -a "Google Chrome" http://localhost:4002/', () => null);
 })
