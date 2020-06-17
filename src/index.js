4e3dff8c9a5212a85ab4656169c3891f42286579
Farhan Poharsae <grobot.developer@gmail.com>
2020-06-17 16:54:43 +0700

    add version text

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-
+import pgk from '../package.json'
 import childProc from 'child_process'
 import server from './createServer'
 
 const port = 4002
 
 server.listen(port, () => {
-
+  console.log('Running version: ' + pgk.version)
   console.log(`🚀 Server ready at http://localhost:${port}`)
   if (process.platform === 'darwin' && process.env.NODE_ENV === 'production') {
     childProc.exec('open -a "Google Chrome" http://localhost:4002/', () => null)
